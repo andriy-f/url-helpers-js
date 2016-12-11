78af41b7db968ac47a6e5683ef9d491acb72fdf2
Andriy Fetsyuk <AndreusAnd@gmail.com>
2016-12-11 19:51:23 +0200

    changed module to UMD

--- url-helpers.js
+++ url-helpers.js
@@ -1,11 +1,36 @@
-/**
- * Combine url fragments
- */
-var stringHelpers = require('string-helpers').stringUtils;
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['string-helpers'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('string-helpers'));
+    } else {
+        // Browser globals (root is window)
+        root['url-helpers'] = factory(root.stringHelpers);
+    }
+} (this, function (stringHelpers) {
 
-exports.combine = function (baseUrl, fragment) {
-    var trimmedBaseUrl = stringHelpers.trimRight(baseUrl, '/'),
-        trimmedFragment = stringHelpers.trimLeft(fragment, '/');
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
 
-    return trimmedBaseUrl + '/' + trimmedFragment;
-};
+    /**
+     * Combine url fragments
+     */
+    function combine(baseUrl, fragment) {
+            var stringUtils = stringHelpers.stringUtils,
+                trimmedBaseUrl = stringUtils.trimRight(baseUrl, '/'),
+                trimmedFragment = stringUtils.trimLeft(fragment, '/');
+
+            return trimmedBaseUrl + '/' + trimmedFragment;
+        }
+ 
+    var urlHelpers = {
+        combine: combine
+    };
+
+    return urlHelpers;
+}));
