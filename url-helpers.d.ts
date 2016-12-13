d8f37cb56ac5840ea8be5d1a1f5d5500f28d99bf
Andriy Fetsyuk <AndreusAnd@gmail.com>
2016-12-13 17:58:03 +0200

    added TypeScript definitions

---
+++ url-helpers.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for url-helpers
+// Project: url-helpers
+// Definitions by: Andriy Fetsyuk
+
+// Declaration for global var of this UMD module
+export as namespace urlHelpers;
+
+export function combine(baseUrl: string, fragment: string);
